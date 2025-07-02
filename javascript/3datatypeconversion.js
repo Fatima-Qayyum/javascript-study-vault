5df20c3362f87fb26edee043f65f08e2cb5fc388
fatima7892 <fatimab7892@gmail.com>
2025-07-02 17:37:59 +0000

    datatype conversions

--- javascript/3datatypeconversion.js
+++ javascript/3datatypeconversion.js
@@ -1,7 +1,49 @@
-let score='33abc';
+/*let score=null;//33abc, true,false,undefined check all these things for better understanding;
 console.log(typeof score);
 console.log(typeof (score));
 
 let valueInNumber=Number(score);
 console.log(typeof valueInNumber);
-console.log(valueInNumber);
+console.log(valueInNumber);*/
+
+
+//"33"-> 33
+// "33abc"->NaN type of NaN is number;
+// true->1  , false->0;
+
+
+/*let isloggedIn=1;
+let boolenaIsLoggedIn=Boolean(isloggedIn);
+console.log(boolenaIsLoggedIn);*/
+
+/*let isloggedIn="";
+let boolenaIsLoggedIn=Boolean(isloggedIn);
+console.log(boolenaIsLoggedIn);*/
+
+
+let isloggedIn="fatima";
+let boolenaIsLoggedIn=Boolean(isloggedIn);
+console.log(boolenaIsLoggedIn);
+
+//1-> ture; 0->false;
+//""->false;
+// "hitesh"->true;
+
+
+
+// convert into string
+
+let someNumber=22;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+//******************************************Operations***********************************
+
+
+
+
+
+
