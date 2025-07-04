64f9b2352339502835a4f7e7900648fbfc564787
fatima7892 <fatimab7892@gmail.com>
2025-07-04 14:45:38 +0500

    change file name

--- javascript/comparision_datatypes.js
+++ javascript/comparision_datatypes.js
@@ -1,1 +1,21 @@
 console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+
+console.log(null>0);// false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+
+console.log(undefined>0);//false
+console.log(undefined<0);//false
+console.log(undefined>=0);//false
+
+
+
+// strict  check in javascript( in strict check it does not only check value but also check its datatypes)
+
+console.log("2"===2)  //false
