adaa57495bdcdd8809b60850b0d9fab5b8674278
fatima7892 <fatimab7892@gmail.com>
2025-07-09 03:33:17 +0500

    arrays methods

--- javascript/array.js
+++ javascript/array.js
@@ -1,20 +1,22 @@
+
+/*-------------------------------------------------ARRAYS IN JAVASCRIPT--------------------------------------------*/
 let cars=["corolla","toyota","bmw"];
-console.log(cars);//[ 'corolla', 'toyota', 'bmw' ]
+console.log(cars);     //[ 'corolla', 'toyota', 'bmw' ]
 cars.push('mehran');
-console.log(cars);//[ 'corolla', 'toyota', 'bmw', 'mehran' ];
+console.log(cars);      //[ 'corolla', 'toyota', 'bmw', 'mehran' ];
 
 
 cars.pop();
-console.log(cars);//[ 'corolla', 'toyota', 'bmw' ]
+console.log(cars);   //[ 'corolla', 'toyota', 'bmw' ]
 
 cars.unshift('mehran');
-console.log(cars);//[ 'mehran', 'corolla', 'toyota', 'bmw' ]
+console.log(cars);   //[ 'mehran', 'corolla', 'toyota', 'bmw' ]
 
 cars.shift();
-console.log(cars);//[ 'corolla', 'toyota', 'bmw' ];
+console.log(cars);    //[ 'corolla', 'toyota', 'bmw' ];
 
 
-//practice question
+//----------------------------------------------practice question-------------------------------------------------------
 let months=['january','july','march','august'];
 months.shift();
 months.shift();
@@ -24,9 +26,12 @@ months.unshift('june');
 months.unshift('july');
 console.log(months);//[ 'july', 'june', 'march', 'august' ]
 
-//idex Of 
+/*--------------------------------------------------Idex Of -----------------------------------------------------------*/
 
 console.log(months.indexOf('august'));//->3
+
+let lang=['c','c++','html','javascript','python','java','c##','sql'];
+console.log(lang. reverse().indexOf('c'));//7
 
 
 
@@ -43,7 +48,7 @@ console.log(months.includes('january'));//it will display false;
 
 
 
-/* --------------------------------------concat--------------------------------------- */
+/* ----------------------------------------------concat-------------------------------------------------------------- */
 
 
 let primary=['red','yellow','blue'];
@@ -61,7 +66,7 @@ console.log(newPrimary);///[ 'red', 'yellow', 'blue', 'orange', 'green', 'violet
 
 
 
- /*------------------------------------slice in arrays*/
+ /*------------------------------------slice in arrays------------------------------------------------------------*/
 
 
  let alphabets=['a','b','c','d','e','f'];
@@ -75,6 +80,35 @@ console.log(newPrimary);///[ 'red', 'yellow', 'blue', 'orange', 'green', 'violet
  let short3=alphabets.slice(-2);
  console.log(short2);    //[ 'c', 'd' ]
  console.log(short3);    //[ 'e', 'f' ]
+
+
+
+ /*---------------------------------------------------------SPLICE--------------------------------------------*/
+
+
+
+ let colors=['red','blue','yellow','green','pink','white','black'];
+ colors.splice(5);
+ console.log(colors);          //[ 'red', 'blue', 'yellow', 'green', 'pink']
+ colors.splice(0,1);
+ console.log(colors);           //[ 'blue', 'yellow', 'green', 'pink' ]
+colors.splice(0,0,'voilet','silver','grey','purple');//['voilet', 'silver', 'grey',   'purple', 'blue',   'yellow' 'green',  'pink']
+console.log(colors);
+
+
+
+let mons=['jan','july','march','august'];
+ mons.splice(0,2,'july','june');
+ console.log(mons);  //[ 'july', 'june', 'march', 'august' ]
+
+
+
+/*-------------------------------------------------*/
+
+
+
+
+
 
 
 
