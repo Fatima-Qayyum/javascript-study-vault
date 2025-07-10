f7508953412fea1673a3e61c4040aa0bc6532975
fatima7892 <fatimab7892@gmail.com>
2025-07-11 00:42:33 +0500

    practic3 arrays and strings

--- javascript/array.js
+++ javascript/array.js
@@ -103,7 +103,99 @@ let mons=['jan','july','march','august'];
 
 
 
-/*------------------------------SORT-------------------*/
+//--------------------------------------------practicejavascriptconcepts-------------------------------------------------------
+
+
+//Qs1. Write a JavaScript program to get the first n elements of an array. [n can be any positive number].For example: for array [7, 9, 0, -2] and n=3
+
+
+let prArray=[7, 9, 0, -2];
+let newPrArray=prArray.slice(0,3);
+console.log(newPrArray);
+
+let array1=prArray.slice(-3);
+console.log(array1);
+
+//Qs6. Write a JavaScript program to check if an element exists in an array or not.
+
+
+let list1=['sugar', 'milk','eggs','tea', 'cofee'
+];
+
+// 1.Create an array of 3 colors. Add one more color to the end of the array and print the updated array.
+
+let colors1=['pink','red','red','yellow'];
+colors1.push('white');
+console.log(colors);
+
+
+
+//Given an array of numbers: [5, 10, 15, 20, 25], print the second and fourth elements.
+let numbers= [5, 10, 15, 20, 25];
+console.log(numbers[0]);
+console.log(numbers[3]);
+
+
+//Remove the last item from the array ["pen", "pencil", "eraser", "sharpener"] and print the result.
+
+let stationary=["pen", "pencil", "eraser", "sharpener"];
+stationary.pop();
+console.log(stationary);
+
+
+//Check if "banana" exists in the array ["apple", "banana", "grape"].If yes, print "Yes", otherwise print "No".
+
+
+let fruits=["apple", "banana", "grape"];
+console.log(fruits.includes('banana'));
+
+
+//Create an array of 4 items. Print the total number of items using .length.
+
+let items=[1, 2 , 3 , 5];
+console.log(items.length);
+
+
+//Create an array of 5 student names. Change the third student's name to "Ali" and print the updated array.
+
+
+let studentsName=['bilal','asim','azeem','asif','salar'];
+studentsName[2]='ali';
+console.log(studentsName);
+
+
+
+//Given an array let numbers = [2, 4, 6, 8, 10];, increase the first number by 5 and print the array.
+numbers = [2, 4, 6, 8, 10];
+numbers[0]=numbers[0]+2;
+console.log(numbers);
+
+
+//Make an empty array called fruits. Add "apple" and "mango" to it using .push(), then print the array.
+
+let fruits2=[];
+fruits2.push('apple');
+fruits2.push('mango');
+console.log(fruits2);
+
+
+//Given let items = ["book", "pen", "notebook", "bag"];,print a new string that says: "I have a bag" using the last element from the array
+
+
+let items2 = ["book", "pen", "notebook", "bag"];
+let message = "I have a " + items2[items2.length - 1];
+console.log(message); // Output: I have a bag
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- javascript/strings.js
+++ javascript/strings.js
@@ -1,3 +1,6 @@
+
+
+/*----------------------------------------------strings-----------------------------------------------------------*/
 // let msg='help! ' trim it and convert it to uppercase
 let msg='help!  ';
 let newMsg=msg.trim().toUpperCase();
@@ -12,3 +15,66 @@ console.log(name.replace('Apna','our'));
 
 
 // in javascript strings are immutable 
+
+//Qs3. Write a JavaScript program to check whether a string is blank or not.
+
+let string1="";
+if ((string1.length)==0){
+    console.log('it is empty');
+}
+else console.log('string has elements');
+
+//Qs4. Write a JavaScript program to test whether the character at the given (character)index is lower case.
+
+let character1='fatima';;
+if (character1[5]==character1[5].toLocaleLowerCase()){
+    console.log('index is in lower case ')
+}
+else{
+    console.log('index is not in lower case');
+}
+
+
+
+//Qs5. Write a JavaScript program to strip leading and trailing spaces from a string.
+
+
+let myname='          fatima    ';
+console.log(myname.trim());
+
+
+//Q Extract the last 3 characters of a given string
+
+let class1='software engineering';
+console.log(class1.slice(-3));
+console.log(class1.slice(class1.length-3));
+
+//Concatenate two strings stored in variables and print the result.
+
+let firstName='Ali';
+let lastName='Abdal';
+console.log(firstName+' '+lastName);
+//Count the number of characters in a given string.
+
+let skill='graphic designing';
+console.log(skill.length);
+
+//Convert a string to uppercase and print the result.
+
+console.log(skill. toLocaleLowerCase());
+
+ //Replace all spaces with hyphens
+ console.log(skill.replace(" ","-"));
+
+
+  //Convert only the last character to uppercase
+
+let str = "apple";
+let lastCharUpper = str[str.length - 1].toUpperCase();
+let result = str.slice(0, str.length - 1) + lastCharUpper;
+console.log(result); // Output: applE
+
+
+
+console.log(list1.includes('sugar'));
+
